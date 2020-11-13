1349255beb96344bb545695f4beb380b45c1d84c
Miku Okajima <okajima.miku@gmail.com>
2020-11-13 23:22:35 +0900

    Refactor code - remove comments

--- src/index.ts
+++ src/index.ts
@@ -1,23 +1,8 @@
 import express from "express";
 import "reflect-metadata";
-// import { User } from "./entity/User";
-// import DatabaseConnectionManager from "./database";
-// import bodyParser from "body-parser";
-import App, { getDefaultApp } from './app';
+import App, { getDefaultApp } from "./app";
 
 const app = express();
-
-//body-parserの設定
-// app.use(bodyParser.urlencoded({ extended: true }));
-// app.use(bodyParser.json());
-
-// const port = process.env.PORT || 3000; // port番号を指定
-
-
-//サーバ起動
-// app.listen(port);
-// console.log("listen on port " + port);
-
 
 const launch = () => {
   const app: App = getDefaultApp();
